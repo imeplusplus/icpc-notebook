312c2bd6fa12a801ee062e555844babeaf694d12
Athos Couto <athos.couto@outlook.com>
2017-02-21 18:31:35 -0300

    Solving minor bugs that costed us hours in subregionals and removing unnecessary variables.

--- graphs/dinic.cpp
+++ graphs/dinic.cpp
@@ -1,4 +1,4 @@
-struct edge {int a, b, c, f;};
+struct edge {int v, c, f;};
 
 int n, s, t, h[N], ptr[N];
 vector<edge> e;
@@ -6,22 +6,22 @@ vector<int> g[N];
 
 void add_edge (int a, int b, int c) {
   int k = e.size();
-  e.push_back({a, b, c, 0});
-  e.push_back({b, a, 0, 0});
+  e.push_back({b, c, 0});
+  e.push_back({a, 0, 0});
   g[a].push_back(k);
   g[b].push_back(k+1);
 }
 
 bool bfs() {
-  memset(h, 63, sizeof(h));
+  memset(h, 0, n * sizeof h[0]);
   queue<int> q;
-  q.push(s);
   h[s] = 0;
+  q.push(s);
   while(!q.empty()) {
     int u = q.front(); q.pop();
     for(int i : g[u]) {
-      int v = e[i].b;
-      if (e[i].f < e[i].c)
+      int v = e[i].v;
+      if (h[v] < 0 and e[i].f < e[i].c)
         q.push(v), h[v] = h[u] + 1;
     }
   }
@@ -31,12 +31,12 @@ bool bfs() {
 int dfs (int u, int flow) {
   if (!flow) return 0;
   if (u == t) return flow;
-  for (int& i = ptr[u]; i < g[u].size(); ++i) {
-    edge& dir = e[g[u][i]], rev = e[g[u][i^1]];
-    int v = dir.b;
-
+  for (int &i = ptr[u]; i < g[u].size(); ++i) {
+    edge &dir = e[g[u][i]], &rev = e[g[u][i]^1];
+    int v = dir.v;
     if (h[v] != h[u] + 1)  continue;
-    int inc = dfs(v, min(flow, dir.c - dir.f));
+    int inc = min(flow, dir.c - dir.f);
+    inc = dfs(v, inc);
     if (inc) {
       dir.f += inc, rev.f -= inc;
       return inc;
@@ -48,7 +48,7 @@ int dfs (int u, int flow) {
 int dinic() {
   int flow = 0;
   while (bfs()) {
-    memset (ptr, 0, n * sizeof ptr[0]);
+    memset(ptr, 0, n * sizeof ptr[0]);
     while (int inc = dfs(s, INF)) flow += inc;
   }
   return flow;
