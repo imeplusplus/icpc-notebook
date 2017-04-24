dd9f16f81b041e1b2cdf62b74f1a5070c00b12e3
Athos Couto <athos.couto@outlook.com>
2017-04-24 20:28:58 -0300

    Made minor changes to 2-SAT to make it more friendly to first-time users.

--- graphs/2-sat.cpp
+++ graphs/2-sat.cpp
@@ -1,19 +1,25 @@
-int x[2*N];
+// 2-SAT - O(V+E)
+// For each variable, we create two nodes in the graph: u and !u
+// If the variable has index i, the index of u and !u are: 2*i and 2*i+1
+int ans[N];
 
-int sat(int u) {
-  x[u] = 1; x[u^1] = 0;
-  for (int v : adj[u]) if (x[v] == -1)
-    sat(v);
+void sat(int u) {
+  ans[u] = 1; ans[u^1] = 0;
+  for (int v : adj[u]) if (ans[v] == -1) sat(v);
 }
 
-// main
-n *= 2;
-// find strongly connected components
+// WARNING: EVERY PROPOSITION MUST BE ADDED WITH IT'S CONTRAPOSITION!
+// u -> v <=> !v -> !u
 
-int ok = 1;
-for (int i = 0; i < n; i+=2) if (scc[i] == scc[i^1]) ok = 0;
+bool sat() {
+  // Use Kosaraju to get SCC in topological order. We assume that:
+  // If node u is ancestor of node v, then scc[u] < scc[v].
 
-cl(x, -1);
-for (int i = 0; i < n; i+=2) if (x[i] == -1) {
-  sat(scc[i] > scc[i^1] ? i : i^1);
+  for (int i = 0; i < 2*n; i+=2)
+    if (scc[i] == scc[i^1]) return 0;
+
+  memset(ans, -1, sizeof ans);
+  for (int i = 0; i < 2*n; i+=2) if (ans[i] == -1) {
+    sat(scc[i] > scc[i^1] ? i : i^1);
+  }
 }
