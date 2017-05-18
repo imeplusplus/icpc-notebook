ec712bbe609c7091e6bb931e055366a934f050ce
Athos Couto <athos.couto@outlook.com>
2017-05-18 12:06:52 -0300

    Made Date code less verbose and less error prone.

--- misc/date.cpp
+++ misc/date.cpp
@@ -1,46 +1,36 @@
-struct Date{
-    int d,m,y;
-    static int month[], monthsum[];
-    Date() : d(1), m(1), y(1) {}
-    Date(int d, int m, int y) : d(d), m(m), y(y) {}
-    Date(int days) : d(1), m(1), y(1) {
-        advance(days);
-    }
-    int mdays() {
-        if(m!=2)
-            return month[m];
-        return 28+bissexto();
-    }
-    int ydays() {
-        return 365+bissexto();
-    }
-    int msum() {
-        return monthsum[m]+(bissexto() and m>2);
-    }
-    bool bissexto() {
-        return (y%4 == 0 and y%100) or (y%400 == 0);
-    }
-    int count() {
-        int ans = d-1 + msum();
+struct Date {
+  int d, m, y;
+  static int mnt[], mntsum[];
 
-        ans += 365*(y-1);
-        ans += (y-1)/4;
-        ans -= (y-1)/100;
-        ans += (y-1)/400;
+  Date() : d(1), m(1), y(1) {}
+  Date(int d, int m, int y) : d(d), m(m), y(y) {}
+  Date(int days) : d(1), m(1), y(1) { advance(days); }
 
-        return ans;
-    }
-    void advance(int days) {
-        days += count();
-        d = m = 1;
-        y = 1 + days/366;
-        days -= count();
-        while(days >= ydays())
-            days -= ydays(), y++;
-        while(days >= mdays())
-            days -= mdays(), m++;
-        d += days;
-    }
+  bool bissexto() { return (y%4 == 0 and y%100) or (y%400 == 0); }
+
+  int mdays() { return mnt[m] + (m == 2)*bissexto(); }
+  int ydays() { return 365+bissexto(); }
+
+  int msum()  { return mntsum[m-1] + (m > 2)*bissexto(); }
+  int ysum()  { return 365*(y-1) + (y-1)/4 - (y-1)/100 + (y-1)/400; }
+
+  int count() { return (d-1) + msum() + ysum(); }
+
+  int day() {
+    int x = y - (m<3);
+    return (x + x/4 - x/100 + x/400 + mntsum[m-1] + d + 6)%7;
+  }
+
+  void advance(int days) {
+    days += count();
+    d = m = 1, y = 1 + days/366;
+    days -= count();
+    while(days >= ydays()) days -= ydays(), y++;
+    while(days >= mdays()) days -= mdays(), m++;
+    d += days;
+  }
 };
-int Date::month[] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
-int Date::monthsum[] = {0,0,31,59,90,120,151,181,212,243,273,304,334,365};
+
+int Date::mnt[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+int Date::mntsum[13] = {};
+for(int i=1; i<13; ++i) Date::mntsum[i] = Date::mntsum[i-1] + Date::mnt[i];
