646f60d55d5e1b60d0347b2214a63a176e35c86a
Matheus Cariús <matheus.carius37@gmail.com>
2017-05-17 21:54:46 -0300

    Improve Date stucture

--- misc/date.cpp
+++ misc/date.cpp
@@ -1,39 +1,27 @@
 struct Date{
     int d,m,y;
+    static int month[], monthsum[];
     Date() : d(1), m(1), y(1) {}
     Date(int d, int m, int y) : d(d), m(m), y(y) {}
+    Date(int days) : d(1), m(1), y(1) {
+        advance(days);
+    }
     int mdays() {
-        switch(m) {
-            case 1:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-            case 10:
-            case 12:
-                return 31;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                return 30;
-            case 2:
-                return 28+bissexto();
-        }
+        if(m!=2)
+            return month[m];
+        return 28+bissexto();
     }
     int ydays() {
         return 365+bissexto();
+    }
+    int msum() {
+        return monthsum[m]+(bissexto() and m>2);
     }
     bool bissexto() {
         return (y%4 == 0 and y%100) or (y%400 == 0);
     }
     int count() {
-        int ans = d-1;
-
-        int aux = m;
-        while(--m >= 1)
-            ans += mdays();
-        m = aux;
+        int ans = d-1 + msum();
 
         ans += 365*(y-1);
         ans += (y-1)/4;
@@ -44,19 +32,15 @@ struct Date{
     }
     void advance(int days) {
         days += count();
-        d = m = y = 1;
-        int l = 1, r = 10000;
-        while(l != r) {
-            y = (l+r+1)/2;
-            if(count() > days)
-                r = y-1;
-            else
-                l = y;
-        }
-        y = l;
+        d = m = 1;
+        y = days/366;
         days -= count();
+        while(days >= ydays())
+            days -= ydays(), y++;
         while(days >= mdays())
             days -= mdays(), m++;
         d += days;
     }
 };
+int Date::month[] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+int Date::monthsum[] = {0,0,31,59,90,120,151,181,212,243,273,304,334,365};
