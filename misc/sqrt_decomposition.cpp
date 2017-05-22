5db29c41a80881dd63a0af6835874af7a0db8f5a
Athos Couto <athos.couto@outlook.com>
2017-05-22 18:19:07 -0300

    Removed unnecessary code from square root decomposition.

--- misc/sqrt_decomposition.cpp
+++ misc/sqrt_decomposition.cpp
@@ -2,30 +2,25 @@
 const int N = 1e5+1, SQ = 500;
 int n, m, v[N];
 
-void add(int p) {}
-void rem(int p) {}
+void add(int p) { /* add value to aggregated data structure */ }
+void rem(int p) { /* remove value from aggregated data structure */ }
 
 struct query { int i, l, r, ans; } qs[N];
 
 bool c1(query a, query b) { return a.l/SQ == b.l/SQ ? a.r < b.r : a.l < b.l; }
 bool c2(query a, query b) { return a.i < b.i; }
 
-int main() {
-  sort(qs, qs+m, c1);
+/* inside main */
+int l = 0, r = -1;
+sort(qs, qs+m, c1);
+for (int i = 0; i < m; ++i) {
+  query &q = qs[i];
+  while (r < q.r) add(v[++r]);
+  while (r > q.r) rem(v[r--]);
+  while (l < q.l) rem(v[l++]);
+  while (l > q.l) add(v[--l]);
 
-  int l = 0, r = -1;
-  for (int i = 0; i < m; ++i) {
-    query &q = qs[i];
-    while (r < q.r) add(v[++r]);
-    while (r > q.r) rem(v[r--]);
-    while (l < q.l) rem(v[l++]);
-    while (l > q.l) add(v[--l]);
-
-    q.ans = /* calculate answer */;
-  }
-
-
-  sort(qs, qs+m, c2);
-  for(int i=0; i<m; ++i) printf("%d\n", qs[i].ans);
-  return 0;
+  q.ans = /* calculate answer */;
 }
+
+sort(qs, qs+m, c2); // sort to original order
