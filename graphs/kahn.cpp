8a94fcdbf14d943700fd4448628501ca197094ed
Filipe Abelha <me@filipeabelha.com>
2017-06-18 20:35:46 -0300

    (1) Fixed wrong variable in loop, (2) now works with 1-indexed graphs, (3) added clarification about inc

--- graphs/kahn.cpp
+++ graphs/kahn.cpp
@@ -1,9 +1,9 @@
 // Kahn - Topological Sort O(V+E)
 vi adj[N];
-int inc[N]; // number of incoming arcs/edges
+int inc[N]; // number of incoming + outcoming arcs/edges
 
 queue<int> q;
-for (int i = 0; k < n; ++i) if (inc[i]<=1) q.push(i);
+for (int i = 1; i <= n; ++i) if (inc[i]<=1) q.push(i);
 
 while (!q.empty()) {
   int u = q.front(); q.pop();
