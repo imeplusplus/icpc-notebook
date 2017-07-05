9f18ad7a3cbe6369b4309bdc8fe478bafcaab084
Athos Couto <athos.couto@outlook.com>
2017-07-05 11:10:12 -0300

    Fixed bugs in LCA.

--- graphs/lca.cpp
+++ graphs/lca.cpp
@@ -1,19 +1,19 @@
 // Lowest Common Ancestor <O(nlogn), O(logn)>
-int par[25][N], h[N];
+int anc[25][N], h[N], rt;
 
-// build (sparse table)
-par[0][1] = 1;
+// build (sancse table)
+anc[0][rt] = rt; // set ancent of the root to itself
 for (int i = 0; (1<<i) < n; ++i)
   for (int j = 1; j <= n; ++j)
-    par[i][j] = par[i-1][par[i-1][j]];
+    anc[i][j] = anc[i-1][anc[i-1][j]];
 
 // query
 int lca(int u, int v) {
   if (h[u] < h[v]) swap(u, v);
-  for (int i = 20; i >= 0; --i) if (h[v]+(1<<i) <= h[u])
-    u = par[u][i];
+  for (int i = 20; i >= 0; --i) if (h[u]-(1<<i) <= h[v])
+    u = anc[i][u];
   if (u == v) return u;
-  for (int i = 20; i >= 0; --i) if (par[i][u] != par[i][u])
-    u = par[i][u], v = par[i][u];
-  return par[0][u];
+  for (int i = 20; i >= 0; --i) if (anc[i][u] != anc[i][v])
+    u = anc[i][u], v = anc[i][v];
+  return anc[0][u];
 }
