e754ceb2e9e615682d531d2303b4802fe6c7aaa4
Naum Azeredo <naumazeredo@gmail.com>
2017-09-05 18:53:07 +0000

    Updated template to ignore debug on OJ

--- template.cpp
+++ template.cpp
@@ -6,11 +6,16 @@ using namespace std;
 #define mp make_pair
 #define pb push_back
 #define cl(x, v) memset((x), (v), sizeof(x))
-#define gcd(x, y) __gcd((x),(y))
+#define gcd(x,y) __gcd((x),(y))
 
-#define db(x) cerr << #x << " == " << x << endl
-#define dbs(x) cerr << x << endl
-#define _ << ", " <<
+#ifndef ONLINE_JUDGE
+  #define db(x) cerr << #x << " == " << x << endl
+  #define dbs(x) cerr << x << endl
+  #define _ << ", " <<
+#else
+  #define db(x)
+  #define dbs(x)
+#endif
 
 typedef long long ll;
 typedef long double ld;
