d0ff1e9d8fee15aabcfc0268488ed56c18bfe767
Athos Couto <athos.couto@outlook.com>
2017-09-06 14:50:11 -0300

    Adapted chinese remainder theorem to extended euclid cointained here.

--- math/chinese.cpp
+++ math/chinese.cpp
@@ -1,6 +1,6 @@
 // finds z (mod p*q) so z = x (mod p) and z = y (mod q)
 ll chinese(ll x, ll p, ll y, ll q) {
-  ll s, r, mod = p*q;
-  euclid(p, q, r, s);
+  ll s, r, d, mod = p*q;
+  euclid(p, q, r, s, d);
   return ((x*(s*q%mod) + y*(r*p%mod) % mod) + mod) % mod;
 }
--- math/primitive_root.cpp
+++ math/primitive_root.cpp
@@ -3,8 +3,7 @@ ll root(ll p) {
   ll n = p-1;
   vector<ll> fact;
 
-  for (int i=2; i*i<=n; ++i)
-    if (n % i == 0) {
+  for (int i=2; i*i<=n; ++i) if (n % i == 0) {
       fact.push_back (i);
       while (n % i == 0) n /= i;
     }
