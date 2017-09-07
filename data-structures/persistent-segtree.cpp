7337bc38912da15a565f53f2167e98ae2c395253
Athos Couto <athos.couto@outlook.com>
2017-09-07 13:24:10 -0300

    Removed unnecessary variables and added comments.

--- data-structures/persistent-segtree.cpp
+++ data-structures/persistent-segtree.cpp
@@ -1,18 +1,21 @@
 // Persistent Segtree
 // Memory: O(n logn)
 // Operations: O(log n)
-int l[N], r[N], st[N], ls[N], rs[N], id[N], stsz;
+
+int li[N], ri[N]; // [li(u), ri(u)] is the interval of node u
+int st[N], lc[N], rc[N]; // Value, left son and right son of node u
+int stsz; // Size of segment tree
 
 // Returns root of initial tree.
 // i and j are the first and last elements of the tree.
 int init(int i, int j) {
   int v = ++stsz;
-  l[v] = i, r[v] = j;
+  li[v] = i, ri[v] = j;
 
   if (i != j) {
-    ls[v] = init(i, (i+j)/2);
-    rs[v] = init((i+j)/2+1, j);
-    st[v] = /* calculate value from rs[v] and ls[v] */;
+    rc[v] = init(i, (i+j)/2);
+    rc[v] = init((i+j)/2+1, j);
+    st[v] = /* calculate value from rc[v] and rc[v] */;
   } else {
     st[v] = /* insert initial value here */;
   }
@@ -22,29 +25,29 @@ int init(int i, int j) {
 
 // Gets the sum from i to j from tree with root u
 int sum(int u, int i, int j) {
-  if (j < l[u] or r[u] < i) return 0;
-  if (i <= l[u] and r[u] <= j) return st[u];
-  return sum(ls[u], i, j) + sum (rs[u], i, j);
+  if (j < li[u] or ri[u] < i) return 0;
+  if (i <= li[u] and ri[u] <= j) return st[u];
+  return sum(rc[u], i, j) + sum (rc[u], i, j);
 }
 
 // Copies node j into node i
 void clone(int i, int j) {
-  l[i] = l[j], r[i] = r[j];
+  li[i] = li[j], ri[i] = ri[j];
   st[i] = st[j];
-  ls[i] = ls[j], rs[i] = rs[j];
+  rc[i] = rc[j], rc[i] = rc[j];
 }
 
 // Sums v to index i from the tree with root u
 int update(int u, int i, int v) {
-  if (i < l[u] or r[u] < i) return u;
+  if (i < li[u] or ri[u] < i) return u;
 
   clone(++stsz, u);
   u = stsz;
-  ls[u] = update(ls[u], i, v);
-  rs[u] = update(rs[u], i, v);
+  rc[u] = update(rc[u], i, v);
+  rc[u] = update(rc[u], i, v);
 
-  if (l[u] == r[u]) st[u] += v;
-  else st[u] = st[ls[u]] + st[rs[u]];
+  if (li[u] == ri[u]) st[u] += v;
+  else st[u] = st[rc[u]] + st[rc[u]];
 
   return u;
 }
