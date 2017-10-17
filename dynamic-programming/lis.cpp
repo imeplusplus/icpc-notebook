93f34b86aeac4df746a27379bf1dec5595457944
Naum Azeredo <naumazeredo@gmail.com>
2017-10-17 05:40:21 +0000

    Added minor guidance on LIS and Kahn

--- dynamic-programming/lis.cpp
+++ dynamic-programming/lis.cpp
@@ -7,6 +7,8 @@ int dp[N], v[N], n, lis;
 
 memset(dp, 63, sizeof dp);
 for (int i=0; i<n; ++i) {
+  // increasing: lower_bound
+  // non-decreasing: upper_bound
   int j = lower_bound(dp, dp+lis, v[i]) - dp;
   dp[j] = min(dp[j], v[i]);
   lis = max(lis, j+1);
--- graphs/kahn.cpp
+++ graphs/kahn.cpp
@@ -1,4 +1,5 @@
 // Kahn - Topological Sort O(V+E)
+
 vi adj[N];
 int inc[N]; // number of incoming arcs/edges
 
@@ -8,6 +9,8 @@ for (int i = 1; i <= n; ++i) if (inc[i]<=1) q.push(i);
 while (!q.empty()) {
   int u = q.front(); q.pop();
   for (int v : adj[u])
+    // undirected graph: inc[v] <= 1
+    // directed graph:   inc[v] == 0
     if (inc[v] > 1 and --inc[v] <= 1)
       q.push(v);
 }
