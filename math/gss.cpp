0eb24c3dd74c6447d23fed7b51b6a61c43c7a1e6
Matheus Cariús <matheus.carius37@gmail.com>
2017-10-31 21:51:33 -0200

    Add Golden Section Search (Ternary Search)

---
+++ math/gss.cpp
@@ -0,0 +1,9 @@
+double gss(double l, double r) {
+    double m1 = r-(r-l)/gr, m2 = l+(r-l)/gr;
+    double f1 = f(m1), f2 = f(m2);
+    while(fabs(l-r)>EPS) {
+        if(f1>f2) l=m1, f1=f2, m1=m2, m2=l+(r-l)/gr, f2=f(m2);
+        else r=m2, f2=f1, m2=m1, m1=r-(r-l)/gr, f1=f(m1);
+    }
+    return l;
+}
