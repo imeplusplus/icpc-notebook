422ed2bbc9d3ed541c3210076304c810ae7d6925
Naum Azeredo <naumazeredo@gmail.com>
2017-11-09 11:51:09 -0200

    Added josephus problem (UFMG)

---
+++ math/josephus.cpp
@@ -0,0 +1,14 @@
+// UFMG
+/* Josephus Problem - It returns the position to be, in order to not die. O(n)*/
+/* With k=2, for instance, the game begins with 2 being killed and then n+2, n+4, ... */
+ll josephus(ll n, ll k) {
+  if(n==1) return 1;
+  else return (josephus(n-1, k)+k-1)%n+1;
+}
+
+/* Another Way to compute the last position to be killed - O(d * log n) */
+ll josephus(ll n, ll d) {
+  ll K = 1;
+  while (K <= (d - 1)*n) K = (d * K + d - 2) / (d - 1);
+  return d * n + 1 - K;
+}
