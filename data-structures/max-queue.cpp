3ebd39b7b8ff10b1a5796d832bc94d41fe9f468b
Naum Azeredo <naumazeredo@gmail.com>
2018-04-09 01:23:11 -0300

    Fix MaxQueue

--- data-structures/max-queue.cpp
+++ data-structures/max-queue.cpp
@@ -1,25 +1,26 @@
 // O(1) complexity for all operations, except for clear,
-//which could be done by creating another deque and using swap
+// which could be done by creating another deque and using swap
 
-struct max_queue{
+struct MaxQueue {
   int plus = 0;
   deque<pair<int, int>> dq;
 
   bool empty() { return dq.size() == 0; }
   void clear() { plus = 0; dq.clear(); }
-  void add(int x){ plus += x; } // Adds x to every element in the queue
-  int max(){ return dq.begin()->first + plus; } // Returns the maximum element in the queue
-  
-  void push(int x){
+  void add(int x) { plus += x; } // Adds x to every element in the queue
+  int max() { return dq.begin()->first + plus; } // Returns the maximum element in the queue
+
+  void push(int x) {
+    x -= plus;
     int amt = 0;
-    while(dq.size() and dq.back().first <= x)
+    while (dq.size() and dq.back().first <= x)
       amt += dq.back().second + 1, dq.pop_back();
-    dq.push_back({ x - plus, amt});
+    dq.push_back({ x, amt });
   }
 
-  void pop(){
-    if(!dq.size()) return;
-    if(!dq.front().second) dq.pop_front();
+  void pop() {
+    if (dq.empty()) return;
+    if (!dq.front().second) dq.pop_front();
     else dq.front().second--;
   }
 };
