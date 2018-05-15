4baedcb4b8170442b711706538d98d6007df1e40
Rebeca Calazans <rebecac.brito@gmail.com>
2018-05-14 23:00:52 -0300

    correct Bellman-Ford

--- graphs/bellman-ford.cpp
+++ graphs/bellman-ford.cpp
@@ -6,5 +6,5 @@ dist[0] = 0;
 for (int i = 0; i < n-1; ++i)
   for (int u = 0; u < n; ++u)
     for (int j = 0; j < adj[u].size(); ++j)
-      v = adj[u][i], w = adjw[u][i],
+      v = adj[u][j], w = adjw[u][j],
       dist[v] = min(dist[v], dist[u]+w);
