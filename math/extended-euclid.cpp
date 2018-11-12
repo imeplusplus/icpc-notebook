9dcfca3c4232a1831fd6eb39a28ceb5c56064783
Mateus de Castro <mateus_lima_castro@hotmail.com>
2018-11-12 19:38:11 -0200

    Update extended-euclid.cpp

--- math/extended-euclid.cpp
+++ math/extended-euclid.cpp
@@ -1,7 +1,7 @@
 // Extended Euclid: gcd(a, b) = x*a + y*b
 // The solutions are:
 // x = x0 + k*b/gcd
-// y = y0 + k*a/gcd
+// y = y0 - k*a/gcd
 void euclid(int a, int b, int &x, int &y, int &d) {
   if (b) euclid(b, a%b, y, x, d), y -= x*(a/b);
   else x = 1, y = 0, d = a;
