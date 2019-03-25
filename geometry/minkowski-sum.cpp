faee70c7ddcc531651c3db20da8d5c53ea01e4ef
João Pedro Xavier <joaopedroaxavier@gmail.com>
2019-03-24 23:30:44 -0300

    Update minkowski-sum.cpp

--- geometry/minkowski-sum.cpp
+++ geometry/minkowski-sum.cpp
@@ -1,3 +1,6 @@
+// Given two polygons, returns the minkowski sum of them.
+//
+// By Agnez
 bool comp(point a, point b){
 	if((a.x > 0 || (a.x==0 && a.y>0) ) && (b.x < 0 || (b.x==0 && b.y<0))) return 1;
 	if((b.x > 0 || (b.x==0 && b.y>0) ) && (a.x < 0 || (a.x==0 && a.y<0))) return 0;
