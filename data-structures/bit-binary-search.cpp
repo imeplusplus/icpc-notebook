4243006557835377cbc22607437e58b1222db8fd
Mateus de Castro <mateus_lima_castro@hotmail.com>
2019-03-25 00:19:26 -0300

    Update bit-binary-search.cpp

--- data-structures/bit-binary-search.cpp
+++ data-structures/bit-binary-search.cpp
@@ -2,7 +2,7 @@
 const int M = 20
 const int N = 1 << M
   
-int binary_search(int val){ //ans => first greater than x
+int lower_bound(int val){
   int ans = 0, sum = 0;
   for(int i = M - 1; i >= 0; i--){
     int x = ans + (1 << i);
