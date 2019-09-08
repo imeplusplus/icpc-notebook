ffa421ccc83cdf4997d02fb93d0fa6de1a098aa9
Henrique Soldi Navarro <42014408+hsnavarro@users.noreply.github.com>
2019-09-07 21:43:53 -0300

    Update basics.cpp

--- math/basics.cpp
+++ math/basics.cpp
@@ -35,6 +35,11 @@ for (int i = 2; i < MOD; ++i)
 f[0] = 0; f[1] = 1;
 for (int i = 2; i <= n; i++) f[i] = f[i-1] + f[i-2];
 
+// Recurrence using matriz
+// h[i+2] = a1*h[i+1] + a0*h[i]
+// [ h[i] h[i-1] ] = [ h[1] h[0] ] * [ a1 1 ] ^ (i - 1)
+//                                   [ a0 0 ] 
+
 // Fibonacci in logarithm time
 // f(2*k) = f(k)*(2*f(k + 1) - f(k))
 // f(2*k + 1) = f(k)^2 + f(k + 1)^2
