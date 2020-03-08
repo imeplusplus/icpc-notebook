db2724acfbc67d9b216b0a54421870fe9575d9f0
Rebeca Calazans <rebecac.brito@gmail.com>
2020-03-07 22:33:59 -0300

    Add mobius for all integers using sieve

--- math/mobius.cpp
+++ math/mobius.cpp
@@ -2,6 +2,20 @@
 // 0 if exists x | n%x² == 0
 // else (-1)^k, k = #(p) | p is prime and n%p == 0
 
+//Calculate Mobius for all integers using sieve
+//O(log(log(n)))
+int mobius() {
+  for(int i = 1; i < N; i++) mob[i] = 1;
+
+  for(ll i = 2; i < N; i++) if(!sieve[i]){
+    for(ll j = i; j < N; j += i) sieve[j] = i, mob[j] *= -1;
+    for(ll j = i*i; j < N; j += i*i) mob[j] = 0;
+  }
+}
+
+/*
+//Calculate Mobius for 1 integer
+//O(sqrt(n))
 int mobius(int n){
   if(n == 1) return 1;
   int p = 0;
@@ -11,5 +25,6 @@ int mobius(int n){
       p++;
       if(n%i == 0) return 0;
     }
-  return p&1 ? 1 : -1;
+  return p&1 ? -1 : 1;
 }
+*/
