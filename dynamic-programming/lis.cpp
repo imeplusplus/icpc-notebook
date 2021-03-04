a4f33154d88e7f8dd0b4f8e516b1c1532c7994a9
Mateus de Castro <samuraiexx@gmail.com>
2021-03-04 02:20:47 -0300

    Add workflow test

--- dynamic-programming/lis.cpp
+++ dynamic-programming/lis.cpp
@@ -3,13 +3,13 @@
 // dp(i) = max j<i { dp(j) | a[j] < a[i] } + 1
 //
 
-int dp[N], v[N], n, lis;
+// int dp[N], v[N], n, lis;
 
 memset(dp, 63, sizeof dp);
-for (int i=0; i<n; ++i) {
+for (int i = 0; i < n; ++i) {
   // increasing: lower_bound
   // non-decreasing: upper_bound
-  int j = lower_bound(dp, dp+lis, v[i]) - dp;
+  int j = lower_bound(dp, dp + lis, v[i]) - dp;
   dp[j] = min(dp[j], v[i]);
-  lis = max(lis, j+1);
+  lis = max(lis, j + 1);
 }
