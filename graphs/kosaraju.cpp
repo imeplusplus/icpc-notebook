1f9fefd87ee349d37d1ec32dd6ff19e26e5c7517
srmacario <srgioreinier@gmail.com>
2021-07-05 18:45:31 -0300

    unconsistency corrections

--- graphs/kosaraju.cpp
+++ graphs/kosaraju.cpp
@@ -1,23 +1,23 @@
 // Kosaraju - SCC O(V+E)
-// For undirected edges uncomment lines related to par, so after dfs it may know its route back through revdfs
+// For undirected graph uncomment lines below
+
 vi adj[N], adjt[N];
 int n, ordn, cnt, vis[N], ord[N], cmp[N];
-// int par[N];
+//int par[N];
 
-void dfs(int s){
-    vis[s] = 1;
-    for(auto v : adj[s]) if(!vis[v]) /*par[v] = s,*/ dfs(v);
-    ord[ordn++] = s;
+void dfs(int u) {
+  vis[u] = 1;
+  for (auto v : adj[u]) if (!vis[v]) dfs(v);
+  // for (auto v : adj[u]) if(!vis[v]) par[v] = u, dfs(v);
+  ord[ordn++] = u;
 }
 
-void revdfs(int s){
-    vis[s] = 0;
-    cmp[s] = cnt;
-    for(auto v : adj[s]) if(vis[v] /*and s != par[v]*/) revdfs(v);
+void dfst(int u) {
+  cmp[x] = cnt, vis[x] = 0;
+  for (auto v : adjt[u]) if (vis[v]) dfst(v);
+  // for (auto v : adj[u]) if(vis[v] and u != par[v]) dfst(v);
 }
 
 // in main
 for (int i = 1; i <= n; ++i) if (!vis[i]) dfs(i);
 for (int i = ordn-1; i >= 0; --i) if (vis[ord[i]]) cnt++, dfst(ord[i]);
-
-/*Undirected SCC tested on Codeforces 1000E (Undirected SCC + diameter)*/
