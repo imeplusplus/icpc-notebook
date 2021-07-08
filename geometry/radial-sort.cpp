c4a21e6291f15811974a9c7c4eec5c90f99aac19
srmacario <srgioreinier@gmail.com>
2021-07-08 18:59:37 -0300

    radial sort added

---
+++ geometry/radial-sort.cpp
@@ -0,0 +1,10 @@
+int above(point p){
+    if(p.y == origin.y) return p.x > origin.x;
+    return p.y > origin.y;
+}
+
+bool cmp(point p, point q){
+    int tmp = above(q) - above(p);
+    if(tmp) return tmp > 0;
+    return p.dir(origin,q) > 0;
+}
