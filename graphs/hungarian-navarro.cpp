55888614ab236b53ed57fd44ea979f012236c62d
hsnavarro <ricksnavarro@gmail.com>
2021-09-01 23:03:49 -0400

    Add hungarian

---
+++ graphs/hungarian-navarro.cpp
@@ -0,0 +1,102 @@
+// Hungarian - O(n^2 * m)
+template<bool is_max = false, class T = int, bool is_zero_indexed = false>
+struct Hungarian {
+	bool swap_coord = false;
+	int lines, cols;
+	T ans;
+
+	vector<int> pairV, way;
+	vector<bool> used;
+	vector<T> pu, pv, minv;
+	vector<vector<T>> cost;
+
+	Hungarian(int _n, int _m) {
+		if (_n > _m) {
+			swap(_n, _m);
+			swap_coord = true;
+		}
+
+		lines = _n + 1, cols = _m + 1;
+
+		clear();
+		cost.resize(lines);
+		for (auto& line : cost) line.assign(cols, 0);
+	}
+
+	void clear() {
+		pairV.assign(cols, 0);
+		way.assign(cols, 0);
+		pv.assign(cols, 0);
+		pu.assign(lines, 0);
+	}
+
+	void update(int i, int j, T val) {
+		if (is_zero_indexed) i++, j++;
+		if (is_max) val = -val;
+		if (swap_coord) swap(i, j);
+
+		assert(i < lines);
+		assert(j < cols);
+
+		cost[i][j] = val;
+	}
+
+	T run() {
+		T _INF = numeric_limits<T>::max();
+		for (int i = 1, j0 = 0; i < lines; i++) {
+			pairV[0] = i;
+			minv.assign(cols, _INF);
+			used.assign(cols, 0);
+			do {
+				used[j0] = 1;
+				int i0 = pairV[j0], j1;
+				T delta = _INF;
+				for (int j = 1; j < cols; j++) {
+					if (used[j]) continue;
+					T cur = cost[i0][j] - pu[i0] - pv[j];
+					if (cur < minv[j]) minv[j] = cur, way[j] = j0;
+					if (minv[j] < delta) delta = minv[j], j1 = j;
+				}
+
+				for (int j = 0; j < cols; j++) {
+					if (used[j]) pu[pairV[j]] += delta, pv[j] -= delta;
+					else minv[j] -= delta;
+				}
+				j0 = j1;
+			} while (pairV[j0]);
+
+			do {
+				int j1 = way[j0];
+				pairV[j0] = pairV[j1];
+				j0 = j1;
+			} while (j0);
+		}
+
+		ans = 0;
+		for (int j = 1; j < cols; j++) if (pairV[j]) ans += cost[pairV[j]][j];
+
+		if (is_max) ans = -ans;
+		if (is_zero_indexed) {
+			for (int j = 0; j + 1 < cols; j++) pairV[j] = pairV[j + 1], pairV[j]--;
+			pairV[cols - 1] = -1;
+		}
+		if (swap_coord) {
+			vector<int> pairV_sub(lines, 0);
+			for (int j = 0; j < cols; j++) if (pairV[j] >= 0) pairV_sub[pairV[j]] = j;
+			swap(pairV, pairV_sub);
+		}
+
+		return ans;
+	}
+};
+
+template <bool is_max = false, bool is_zero_indexed = false>
+struct HungarianMult : public Hungarian<is_max, long double, is_zero_indexed> {
+	using super = Hungarian<is_max, long double, is_zero_indexed>;
+
+	HungarianMult(int _n, int _m) : super(_n, _m) {}
+
+	void update(int i, int j, long double x) {
+		super::update(i, j, log2(x));
+	}
+};
