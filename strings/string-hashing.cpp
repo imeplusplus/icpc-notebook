003fe805e7af1609d7aba27e575eeaca346591be
Henrique Soldi Navarro <42014408+hsnavarro@users.noreply.github.com>
2021-10-23 22:19:11 -0400

    Create string-hashing.cpp

---
+++ strings/string-hashing.cpp
@@ -0,0 +1,46 @@
+// String Hashing
+// Rabin Karp - O(n + m)
+
+// max size txt + 1
+const int N = 1e6 + 5;
+
+// lowercase letters p = 31 (remember to do s[i] - 'a' + 1)
+// uppercase and lowercase letters p = 53 (remember to do s[i] - 'a' + 1)
+// any character p = 313
+
+const int MOD = 1e9+9;
+ull h[N], p[N];
+
+int cnt;
+
+
+void build(string &s) {
+  p[0] = 1, p[1] = 313;
+  for(int i = 1; i <= s.size(); i++) {
+    h[i] = ((p[1]*h[i-1]) % MOD + s[i-1]) % MOD;
+    p[i] = (p[1]*p[i-1]) % MOD;
+  }
+}
+
+// 1-indexed
+ull fhash(int l, int r) {
+  return (h[r] - ((h[l-1]*p[r-l+1]) % MOD) + MOD) % MOD;
+}
+
+ull shash(string &pt) {
+  ull h = 0;
+  for(int i = 0; i < pt.size(); i++) 
+    h = ((h*p[1]) % MOD + pt[i]) % MOD;
+  return h;
+}
+
+void rabin_karp(string &s, string &pt) {
+  build(s);
+  ull hp = shash(pt);
+  for(int i = 0, m = pt.size(); i + m <= s.size(); i++) {
+    if(fhash(i+1, i+m) == hp) {
+      // match at i
+      cnt++;
+    }
+  }
+}
