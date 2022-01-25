250bef23949c16a5d4f9815083fa8d7bd9c96885
Davi-Holanda <37756892+Davi-Holanda@users.noreply.github.com>
2022-01-25 10:40:01 -0300

    Update discrete-log.cpp

--- math/discrete-log.cpp
+++ math/discrete-log.cpp
@@ -1,5 +1,7 @@
-// solve the discrete equation a^k = m mod(p).
-// i.e find k such that the equation above is satisfied.
+// O(sqrt(p))
+// Find integer k >= 0 such that a^k = m mod(p) or return -1 if not possible
+// Works just for p prime and gives one arbitrary solution.
+// For a stronger (and larger) algorithm use the second function.
 ll discrete_log(ll a, ll m, ll p) {
   unordered_map<ll, ll> babystep;
 
@@ -23,6 +25,61 @@ ll discrete_log(ll a, ll m, ll p) {
     gstep = (gstep * an) % p;
   }
 
-  // returns -1 if there isn't any possible value for the answer.
   return -1;
+}
+
+// O(sqrt(m))
+// Solve c * a^x = b mod(m) for integer x >= 0.
+// Return {x, y} where x and y are solutions.
+// If {-1, -1} it means that there are no solutions.
+// If {x, -1} it means that x is the only solution.
+// If {x, y} it means that x + k * (y - x) for k >= 0 are all the solutions.
+// Works for any integer values of c, a, b and positive m
+pll discrete_log(ll c, ll a, ll b, ll m){
+  pll r = {-1, -1};
+  c = ((c % m) + m) % m, a = ((a % m) + m) % m, b = ((b % m) + m) % m;
+  if(c == b)
+    r.st = 0;
+  
+  ll g = __gcd(a, m);
+  if(b % g) return r;
+  
+  if(g > 1){
+    pll h = discrete_log(c * a / g, a, b / g, m / g);
+    if(r.st == 0) r.nd = h.st + (h.st >= 0);
+    else r = {h.st + (h.st >= 0), h.nd + (h.nd >= 0)};
+    return r;
+  }
+  
+  unordered_map<ll, ll> babystep[2];
+  ll n = 1, an = a % m;
+  
+  // set n to the ceil of sqrt(m):
+  while(n * n < m) n++, an = (an * a) % m;
+
+  // babysteps:
+  ll bstep = b;
+  for(ll i = 0; i <= n; i++){
+    if(babystep[0].find(bstep) != babystep[0].end())
+    	babystep[1][bstep] = babystep[0][bstep];
+    babystep[0][bstep] = i;
+    bstep = (bstep * a) % m;
+  }
+
+  // giantsteps:
+  ll gstep = c * an % m;
+  for(ll i = 1; i <= n; i++){
+    for(int j = 0; j < 2; j++){
+      if(babystep[j].find(gstep) != babystep[j].end()){
+        ll h = n * i - babystep[j][gstep];
+        if(r.st < 0 || r.st > h){
+          r.nd = r.st;
+          r.st = h;
+	    }
+	    else if(r.st != h && (r.nd < 0 || r.nd > h)) r.nd = h;
+      }
+    }
+	gstep = (gstep * an) % m;
+  }
+  return r;
 }
