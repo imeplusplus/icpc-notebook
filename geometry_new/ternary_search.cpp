86e7ec282e4c3db8abb675ebe849ccd41eaedb86
srmacario <sergioreinier@gmail.com>
2022-02-03 00:03:29 -0300

    ternary search fixed and comments added

--- geometry_new/ternary_search.cpp
+++ geometry_new/ternary_search.cpp
@@ -6,12 +6,14 @@ ll ternary_search(ll l, ll r){
         ll m1 = (l+r)/2;
         ll m2 = (l+r)/2 + 1;
         ll f1 = f(m1), f2 = f(m2);   
+        //if(f1 > f2) l = m1;
         if (f1 < f2) l = m1;
         else r = m2;
     }
     ll ans = 0;
     for(int i = l; i <= r; i++){
         ll tmp = f(i);
+        //ans = min(ans, tmp);
         ans = max(ans, tmp);
     }
     return ans;
@@ -20,9 +22,10 @@ ll ternary_search(ll l, ll r){
 //Faster version - 300 iteratons up to 1e-6 precision 
 double ternary_search(double l, double r, int No = 300){
     // for(int i = 0; i < No; i++){
-    while(r - l < EPS){
+    while(r - l > EPS){
         double m1 = l + (r - l) / 3;
         double m2 = r - (r - l) / 3;
+        // if (f(m1) > f(m2))
         if (f(m1) < f(m2))
             l = m1;
         else
