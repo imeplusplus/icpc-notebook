b0c53f42b03b2e2ec99d380eaea5a0a23ff5ae6f
Henrique Soldi Navarro <42014408+hsnavarro@users.noreply.github.com>
2022-02-14 21:14:58 -0300

    Update segtree.cpp

--- data-structures/segtree.cpp
+++ data-structures/segtree.cpp
@@ -1,7 +1,9 @@
-// Segment Tree
-int st[4*N], v[N], lz[4*N];
+// Segment Tree (Range Query and Range Update)
+// Update and Query - O(log n)
 
-void build(int p, int l, int r) {
+int n, v[N], lz[4*N], st[4*N];
+
+void build(int p = 1, int l = 1, int r = n) {
   if (l == r) { st[p] = v[l]; return; }
   build(2*p, l, (l+r)/2);
   build(2*p+1, (l+r)/2+1, r);
@@ -18,20 +20,20 @@ void push(int p, int l, int r) {
   }
 }
 
-int query(int p, int l, int r, int i, int j) {
+int query(int i, int j, int p = 1, int l = 1, int r = n) {
   push(p, l, r);
-  if (r < i or l > j) return INF; // RMQ -> INF, RSQ -> 0
-  if (l >= i and r <= j) return st[p];
-  return min(query(2*p, l, (l+r)/2, i, j),
-             query(2*p+1, (l+r)/2+1, r, i, j));
+  if (l > j or r < i) return INF; // RMQ -> INF, RSQ -> 0
+  if (l >= i and j >= r) return st[p];
+  return min(query(i, j, 2*p, l, (l+r)/2),
+             query(i, j, 2*p+1, (l+r)/2+1, r));
   // RMQ -> min/max, RSQ -> +
 }
 
-void update(int p, int l, int r, int i, int j, int v) {
+void update(int i, int j, int v, int p = 1, int l = 1, int r = n) {
   push(p, l, r);
-  if (r < i or l > j) return;
-  if (l >= i and r <= j) { lz[p] = v; push(p, l, r); return; }
-  update(2*p, l, (l+r)/2, i, j, v);
-  update(2*p+1, (l+r)/2+1, r, i, j, v);
+  if (l > j or r < i) return;
+  if (l >= i and j >= j) { lz[p] = v; push(p, l, r); return; }
+  update(i, j, v, 2*p, l, (l+r)/2);
+  update(i, j, v, 2*p+1, (l+r)/2+1, r);
   st[p] = min(st[2*p], st[2*p+1]); // RMQ -> min/max, RSQ -> +
 }
