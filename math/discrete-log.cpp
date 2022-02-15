a8cee84f6d846d1157f8c85b018796280fdea8e7
Davi-Holanda <37756892+Davi-Holanda@users.noreply.github.com>
2022-02-15 13:00:09 -0300

    Update discrete-log.cpp

--- math/discrete-log.cpp
+++ math/discrete-log.cpp
@@ -4,37 +4,37 @@
 // If all solutions needed, solve c * a^x = b mod(m) and (a*b) * a^y = b mod(m)
 // x + k * (y + 1) for k >= 0 are all solutions
 ll discrete_log(ll c, ll a, ll b, ll m){
-  c = ((c % m) + m) % m, a = ((a % m) + m) % m, b = ((b % m) + m) % m;
-  if(c == b)
-    return 0;
+	c = ((c % m) + m) % m, a = ((a % m) + m) % m, b = ((b % m) + m) % m;
+	if(c == b)
+		return 0;
   
-  ll g = __gcd(a, m);
-  if(b % g) return -1;
+	ll g = __gcd(a, m);
+	if(b % g) return -1;
   
-  if(g > 1){
-  	ll r = discrete_log(c * a / g, a, b / g, m / g);
-  	return r + (r >= 0);
-  }
+	if(g > 1){
+		ll r = discrete_log(c * a / g, a, b / g, m / g);
+		return r + (r >= 0);
+	}
   
-  unordered_map<ll, ll> babystep;
-  ll n = 1, an = a % m;
+	unordered_map<ll, ll> babystep;
+	ll n = 1, an = a % m;
   
-  // set n to the ceil of sqrt(m):
-  while(n * n < m) n++, an = (an * a) % m;
+	// set n to the ceil of sqrt(m):
+	while(n * n < m) n++, an = (an * a) % m;
 
-  // babysteps:
-  ll bstep = b;
-  for(ll i = 0; i <= n; i++){
-    babystep[bstep] = i;
-    bstep = (bstep * a) % m;
-  }
+	// babysteps:
+	ll bstep = b;
+	for(ll i = 0; i <= n; i++){
+		babystep[bstep] = i;
+		bstep = (bstep * a) % m;
+	}
 
-  // giantsteps:
-  ll gstep = c * an % m;
-  for(ll i = 1; i <= n; i++){
-    if(babystep.find(gstep) != babystep.end())
-    	return n * i - babystep[gstep];
+	// giantsteps:
+	ll gstep = c * an % m;
+	for(ll i = 1; i <= n; i++){
+		if(babystep.find(gstep) != babystep.end())
+			return n * i - babystep[gstep];
 		gstep = (gstep * an) % m;
-  }
-  return -1;
+	}
+	return -1;
 }
