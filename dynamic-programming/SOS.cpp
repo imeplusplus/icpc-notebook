c0a01a8073e684584972810d2bbea87ab52b4a89
Davi-Holanda <37756892+Davi-Holanda@users.noreply.github.com>
2022-03-09 20:19:22 -0300

    Update SOS.cpp

--- dynamic-programming/SOS.cpp
+++ dynamic-programming/SOS.cpp
@@ -4,6 +4,6 @@
 for(int i = 0; i < (1 << N); i++)
 	F[i] = A[i];
 for(int i = 0; i < N; i++)
-    for(int j = 0; j < (1 << N); j++)
-	    if(j & (1 << i))
-		    F[j] += F[j ^ (1 << i)];
+	for(int j = 0; j < (1 << N); j++)
+		if(j & (1 << i))
+			F[j] += F[j ^ (1 << i)];
