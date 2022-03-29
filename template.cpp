6d5b8b6f9a77fdbb6edfdd5156dbabdd4ad8a9dd
srmacario <sergioreinier@gmail.com>
2022-03-28 22:25:22 -0300

    notebook updated

--- template.cpp
+++ template.cpp
@@ -4,7 +4,6 @@ using namespace std;
 #define st first
 #define nd second
 #define mp make_pair
-#define pb push_back
 #define cl(x, v) memset((x), (v), sizeof(x))
 #define gcd(x,y) __gcd((x),(y))
 
@@ -23,13 +22,16 @@ typedef long double ld;
 typedef pair<int, int> pii;
 typedef pair<int, pii> piii;
 typedef pair<ll, ll> pll;
-typedef vector<int> vi;
+typedef pair<ll, pll> plll;
 
 const ld EPS = 1e-9, PI = acos(-1.);
-const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;;
 const int N = 1e5+5;
 
 int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
   //freopen("in", "r", stdin);
   //freopen("out", "w", stdout);
   return 0;
