156c35b2237461ca3f73e4b126ff1794bd99acc0
Davi-Holanda <37756892+Davi-Holanda@users.noreply.github.com>
2022-09-14 21:34:38 -0300

    Update bit2d.cpp

--- data-structures/bit2d.cpp
+++ data-structures/bit2d.cpp
@@ -1,50 +1,58 @@
-// Fenwick Tree 2D / Binary Indexed Tree 2D
-int bit[N][N];
+// Thank you for the code tfg!
+template<class T = int>
+struct Bit2D{
+	vector<T> ord;
+	vector<vector<T>> fw, coord;
+	
+	// if range qrys/upds remember to build with x + 1, x - 1, y + 1 and y - 1 already in pts
+	Bit2D(vector<pair<T, T>> pts) {
+		sort(pts.begin(), pts.end());
+		for(auto a : pts)
+			if(ord.empty() || a.first != ord.back())
+				ord.push_back(a.first);
+		fw.resize(ord.size() + 1);
+		coord.resize(fw.size());
+		
+		for(auto &a : pts)
+			swap(a.first, a.second);
+		sort(pts.begin(), pts.end());
+		for(auto &a : pts){
+			swap(a.first, a.second);
+			for(int on = std::upper_bound(ord.begin(), ord.end(), a.first) - ord.begin(); on < fw.size(); on += on & -on)
+				if(coord[on].empty() || coord[on].back() != a.second)
+					coord[on].push_back(a.second);
+		}
+		
+		for(int i = 0; i < fw.size(); i++)
+			fw[i].assign(coord[i].size() + 1, 0);
+	}
 
-void add(int i, int j, int v) {
-  for (; i < N; i+=i&-i)
-    for (int jj = j; jj < N; jj+=jj&-jj)
-      bit[i][jj] += v;
-}
+	// point upd
+	void upd(T x, T y, T v){
+		for(int xx = upper_bound(ord.begin(), ord.end(), x) - ord.begin(); xx < fw.size(); xx += xx & -xx)
+			for(int yy = upper_bound(coord[xx].begin(), coord[xx].end(), y) - coord[xx].begin(); yy < fw[xx].size(); yy += yy & -yy)
+				fw[xx][yy] += v;
+	}
+	
+	// point qry
+	T qry(T x, T y){
+		T ans = 0;
+		for(int xx = upper_bound(ord.begin(), ord.end(), x) - ord.begin(); xx > 0; xx -= xx & -xx)
+			for(int yy = upper_bound(coord[xx].begin(), coord[xx].end(), y) - coord[xx].begin(); yy > 0; yy -= yy & -yy)
+				ans += fw[xx][yy];
+		return ans;
+	}
 
-int query(int i, int j) {
-  int res = 0;
-  for (; i; i-=i&-i)
-    for (int jj = j; jj; jj-=jj&-jj)
-      res += bit[i][jj];
-  return res;
-}
+	// range qry
+	T qry(T x1, T y1, T x2, T y2){
+		return qry(x2, y2) - qry(x2, y1 - 1) - qry(x1 - 1, y2) + qry(x1 - 1, y1 - 1);
+	}
 
-// Whole BIT 2D set to 1
-void init() {
-  cl(bit,0);
-  for (int i = 1; i <= r; ++i)
-    for (int j = 1; j <= c; ++j)
-      add(i, j, 1);
-}
-
-// Return number of positions set
-int query(int imin, int jmin, int imax, int jmax) {
-  return query(imax, jmax) - query(imax, jmin-1) - query(imin-1, jmax) + query(imin-1, jmin-1);
-}
-
-// Find all positions inside rect (imin, jmin), (imax, jmax) where position is set
-void proc(int imin, int jmin, int imax, int jmax, int v, int tot) {
-  if (tot < 0) tot = query(imin, jmin, imax, jmax);
-  if (!tot) return;
-
-  int imid = (imin+imax)/2, jmid = (jmin+jmax)/2;
-  if (imin != imax) {
-    int qnt = query(imin, jmin, imid, jmax);
-    if (qnt) proc(imin, jmin, imid, jmax, v, qnt);
-    if (tot-qnt) proc(imid+1, jmin, imax, jmax, v, tot-qnt);
-  } else if (jmin != jmax) {
-    int qnt = query(imin, jmin, imax, jmid);
-    if (qnt) proc(imin, jmin, imax, jmid, v, qnt);
-    if (tot-qnt) proc(imin, jmid+1, imax, jmax, v, tot-qnt);
-  } else {
-    // single position set!
-    // now process position!!!
-    add(imin, jmin, -1);
-  }
-}
+	// range upd
+	void upd(T x1, T y1, T x2, T y2, T v) {
+		upd(x1, y1, v);
+		upd(x1, y2 + 1, -v);
+		upd(x2 + 1, y1, -v);
+		upd(x2 + 1, y2 + 1, v);
+	}
+};
