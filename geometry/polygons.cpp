db5b7b2c8ebe2a857a997c20000efc82b61af5ee
srgioreinier@gmail.com <srgioreinier@gmail.com>
2023-09-04 21:05:55 -0300

    removing graham hull

--- geometry/polygons.cpp
+++ geometry/polygons.cpp
@@ -1,55 +1,6 @@
 #include "basics.cpp"
 #include "lines.cpp"
 
-//Graham scan NOT TESTED ENOUGH, not safe, prefer monotone chain!
-point origin;
-
-int above(point p){
-    if(p.y == origin.y) return p.x > origin.x;
-    return p.y > origin.y;
-}
-
-bool cmp(point p, point q){
-    int tmp = above(q) - above(p);
-    if(tmp) return tmp > 0;
-    return p.dir(origin,q) > 0;
-    //Be Careful: p.dir(origin,q) == 0
-}
-
-// Graham Scan O(nlog(n))
-vector<point> graham_hull(vector<point> pts) {
-    vector<point> ch(pts.size());
-    point mn = pts[0];
-
-    for(point p : pts) if (p.y < mn.y or (p.y == mn.y and p.x < mn.x)) mn = p;
-
-    origin = mn;
-    sort(pts.begin(), pts.end(), cmp);
-
-    int n = 0;
-
-    // IF: Convex hull without collinear points
-    // for(point p : pts) {
-    //     while (n > 1 and ch[n-1].dir(ch[n-2], p) < 1) n--;
-    //     ch[n++] = p;
-    // }
-
-    //ELSE IF: Convex hull with collinear points
-    for(point p : pts) {
-    while (n > 1 and ch[n-1].dir(ch[n-2], p) < 0) n--;
-    ch[n++] = p;
-    }
-
-    /*this part not safe
-    for(int i=pts.size()-1; i >=1; --i)
-    if (n > 1 and pts[i] != ch[n-1] and !pts[i].dir(pts[0], ch[n-1]))
-        ch[n++] = pts[i];*/
-    // END IF
-
-    ch.resize(n);
-    return ch;
-}
-
 //Monotone chain O(nlog(n))
 #define REMOVE_REDUNDANT
 #ifdef REMOVE_REDUNDANT
@@ -60,7 +11,7 @@ bool between(const point &a, const point &b, const point &c) {
 
 //new change: <= 0 / >= 0 became < 0 / > 0 (yet to be tested)
 
-void monotone_hull(vector<point> &pts) {
+void convex_hull(vector<point> &pts) {
     sort(pts.begin(), pts.end());
     pts.erase(unique(pts.begin(), pts.end()), pts.end());
     vector<point> up, dn;
