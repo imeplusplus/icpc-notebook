255229db01f9e4927b6606ba1a7295f941c5f068
srmacario <sergioreinier@gmail.com>
2024-11-07 22:22:37 -0300

    chore: update radial sort documentation

--- geometry/radial_sort.cpp
+++ geometry/radial_sort.cpp
@@ -1,10 +1,17 @@
 #include "basics.cpp"
-point origin;
 
 /*
 	below < above
 	order: [pi, 2 * pi)
+
+	Example:
+	[(1, 0), (1, 1), (0, 1), (-1, 0), (-1, -1), (0, -1)]
+		after sort =>
+	[(-1, 0), (-1, -1), (0, -1), (1, 0), (1, 1), (0, 1)]
 */
+
+// Don't forget to change origin!
+point origin;
 
 int above(point p){
 	if(p.y == origin.y) return p.x > origin.x;
@@ -14,6 +21,7 @@ int above(point p){
 bool cmp(point p, point q){
 	int tmp = above(q) - above(p);
 	if(tmp) return tmp > 0;
+
 	return p.dir(origin,q) > 0;
 	//Be Careful: p.dir(origin,q) == 0
 }
