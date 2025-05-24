d6db5cb0e077ed674fd16828f3c1e452a222d103
mateus sze cosenza <80402245+cosenza987@users.noreply.github.com>
2025-05-23 21:51:58 -0300

    Update gauss_elim_prime.cpp

--- math/gauss_elim_prime.cpp
+++ math/gauss_elim_prime.cpp
@@ -1,23 +1,30 @@
-//ll A[N][M+1], X[M]
-
-for(int j=0; j<m; j++) { //collumn to eliminate
-	int l = j;
-	for(int i=j+1; i<n; i++) //find nonzero pivot
-		if(A[i][j]%p)
-			l=i;
-	for(int k = 0; k < m+1; k++) { //Swap lines
-		swap(A[l][k],A[j][k]);
-	}
-	for(int i = j+1; i < n; i++) { //eliminate column
-		ll t=mulmod(A[i][j],inv(A[j][j],p),p);
-		for(int k = j; k < m+1; k++)
-			A[i][k]=(A[i][k]-mulmod(t,A[j][k],p)+p)%p;
-	}
+for(int k = 0; k < m; k++) {
+    int l = k;
+    while(l < n and !v[l][k].x) l++;
+    if(l == n) {
+        v[n][k] = 1;
+        l = n++;
+    }
+    swap(v[k], v[l]);
+    mint t = v[k][k];
+    for(int j = 0; j <= m; j++) {
+        v[k][j] /= t;
+    }
+    for(int i = 0; i < n; i++) {
+        if(i == k) continue;
+        mint q = v[i][k];
+        for(int j = 0; j <= m; j++) {
+            v[i][j] -= v[k][j] * q;
+        }
+    }
 }
-
-for(int i = m-1; i >= 0; i--) { //solve triangular system
-	for(int j = m-1; j > i; j--)
-		A[i][m] = (A[i][m] - mulmod(A[i][j],X[j],p)+p)%p;
-		X[i] = mulmod(A[i][m],inv(A[i][i],p),p);
+for(int i = m; i < n; i++) {
+    mint s = 0;
+    for(int k = 0; k < m; k++) {
+        s += v[i][k] * v[k][m];
+    }
+    if(s != v[i][m]) {
+        cout << -1 << "\n";
+        return 0;
+    }
 }
-
